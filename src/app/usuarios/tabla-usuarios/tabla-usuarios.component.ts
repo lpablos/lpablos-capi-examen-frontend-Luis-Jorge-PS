e979873c75fc393c0cfc382dc8a8ce29a06dee3e
Lpablo <lpablo@acer.com>
2024-02-02 21:48:08 -0600

    pintar los datos en una tabla

--- src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
+++ src/app/usuarios/tabla-usuarios/tabla-usuarios.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api-service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tabla-usuarios',
   templateUrl: './tabla-usuarios.component.html',
   styleUrl: './tabla-usuarios.component.css'
 })
-export class TablaUsuariosComponent {
+export class TablaUsuariosComponent implements OnInit {
+  datos:any[];
 
+  constructor(private service: ApiService){
+    this.datos = [];
+  
+  }
+  ngOnInit(): void {
+    this.service.getAll().subscribe((data)=>{
+      this.datos = data;
+      console.log("Es el request", this.datos);
+    })
+  }
 }
